938db1fe5bc61db87727f09d2f35747cda3c8197
Brian Shirai <brixen@gmail.com>
2026-09-24 12:15:34 -0700

    Add zine.

---
+++ Formula/zine.rb
@@ -0,0 +1,14 @@
+class Mytool < Formula
+  desc "Zine static site generator written in Zig"
+  homepage "https://github.com/kristoff-it"
+  url "https://github.com/kristoff-it/zine/releases/download/v0.14.0/aarch64-macos.zip"
+  sha256 "5f2222d88343c4dc474d8c6f499bf754aa2766b2a7bd518abf7bf331645d2b7c"
+
+  def install
+    bin.install "aarch64-macos.zip"
+  end
+
+  test do
+    system "#{bin}/mytool", "--version"
+  end
+end
